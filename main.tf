0bb54df0dcf207035c96717318453f2b69619a51
Tiago Brito <e3205@itsector.pt>
2023-11-07 17:38:46 -0300

    feat: lambda 1

---
+++ main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "lanchonetedarua"
+
+    workspaces {
+      name = "lanchonete-da-rua-iac"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+## Lambda
+resource "aws_iam_role" "lambda_role" {
+name   = "iamRoleLambdaFunctionRole"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path         = "/"
+ description  = "AWS IAM Gerenciamento da politica das lambdas"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+## Anexar política do IAM à função do IAM
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+function_name                  = "LanchoneteDaRua_Token_Lambda_Function"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "python3.8"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
